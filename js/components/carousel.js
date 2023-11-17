e7edec694c37673867657b84ce842b16af04cec1
rms43 <rafa.malassise@gmail.com>
2023-11-16 23:14:14 -0300

    Make a Carousel About

---
+++ js/components/carousel.js
@@ -0,0 +1,15 @@
+const btnNext = document.querySelector('.button-arrow.-next');
+const btnBack = document.querySelector('.button-arrow.-back');
+const elements = document.querySelector('.elements');
+const pixel = 50;
+let step = 0;
+
+btnNext.addEventListener("click", function() {
+    step += pixel;
+    elements.style = `transform: translateX(${step}px)`;
+});
+
+btnBack.addEventListener('click', function() {
+    step -= pixel;
+    elements.style = `transform: translateX(${step}px)`;
+}); 
